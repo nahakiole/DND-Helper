fe11918424841c863deb113c54d8a070cec10c1a
Robin Glauser <robin.glauser@gmail.com>
2015-08-16 11:44:39 +0200

    Adding tag autocomplete

--- js/main.js
+++ js/main.js
@@ -17,12 +17,29 @@ dndApp.controller('tableController', function ($scope) {
     }
   ];
 
+  $scope.tags = [
+    'vergiftet',
+    'feuer',
+    'betäubt'
+  ];
+
   $scope.addEnemy = function(name){
       $scope.enemies.push( JSON.parse(JSON.stringify(   $.grep($scope.enemyTypes, function(e){ return e.name == name; })[0])) );
   };
 
   $scope.instaKill = function(enemy){
     $scope.enemies.splice(enemy,1);
+  }
+
+  $scope.getTags = function(name){
+    var regexp = new RegExp(name, "gi");
+    return $scope.tags.filter(
+      function(tag){return tag.match(regexp);}
+    ).sort(function(a, b){
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+});
   }
 
 });
