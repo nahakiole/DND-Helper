ba08bea85a62b695fe90a2813ce3880f6c755b72
Manuel Meister <news.manuelsworld@gmail.com>
2018-12-16 13:38:24 +0100

    Created crawler script to output soundslist as a json: ``` [   {     "id": 1,     "category": "Monsters Big",     "title": "Roaahrr",     "length": "6.5s",     "loop": true,     "src": "sounds/Monsters%20Big/roar.wav"   },   {     "id": 2,     "category": "Fight",     "title": "Field of Heroes",     "length": "1m 3s",     "loop": false,     "src": "sounds/Fight/157_Field_of_Heroes.mp3"   } ] ```

---
+++ inc/sounds.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuelmeister
+ * Date: 16.12.18
+ * Time: 12:04
+ */
+
+define( 'IGNORE_FILES', [ '.', '..' ] );
+define( 'DIR_ROOT', 'sounds/' );
+
+require '../vendor/autoload.php';
+
+use Vistag\HumanReadable\ReadableSeconds;
+
+try {
+	$dir   = scandir( '../' . DIR_ROOT );
+	$items = array_diff( $dir, IGNORE_FILES );
+
+	$id     = 0;
+	$sounds = [];
+
+	$id3engine = new getID3();
+
+
+	function convert_to_url($path) {
+		$exploded = explode('/',$path);
+		$return = [];
+		foreach ($exploded as $item) {
+			$return[] = rawurlencode($item);
+		}
+		return join('/',$return);
+	}
+
+	/**
+	 * Loops through given directory
+	 *
+	 * @param $items
+	 * @param $category
+	 * @param $dir_path
+	 * @param $id3engine getID3
+	 * @param $id
+	 * @param $sounds
+	 */
+	function loop_dir( $items, $category, $dir_path, $id3engine, &$id, &$sounds ): void {
+		foreach ( $items as $item ) {
+			if ( is_dir( '../' . $dir_path . $item ) ) {
+				$local_dirpath = $dir_path . $item . '/';
+				$local_dir     = scandir( '../' . $local_dirpath );
+				$local_items   = array_diff( $local_dir, IGNORE_FILES );
+				loop_dir( $local_items, $item, $local_dirpath, $id3engine, $id, $sounds );
+			} else {
+				$info     = $id3engine->analyze( '../' . $dir_path . $item );
+				$sounds[] = [
+					'id'       => $id ++,
+					'category' => rawurldecode($category),
+					'title'    => $info['tags']['riff']['title'][0],
+					'length'   => (new ReadableSeconds(round($info['playtime_seconds'],1)))->long(),
+					'loop'     => strpos( $item, '_loop' ) !== false,
+					'src'      => convert_to_url($dir_path . $item)
+				];
+			}
+		}
+	}
+
+	loop_dir( $items, 'Uncategorized', DIR_ROOT, $id3engine, $id, $sounds );
+
+	header( 'Content-Type: application/json' );
+	echo( json_encode( $sounds ) );
+} catch ( Exception $e ) {
+	header( 'Sorry: ' . $e->getTraceAsString(), false, 500 );
+	die();
+}
